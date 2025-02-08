dd0199810e0f2f08be4a1d798c5eb560c5b84a45
Daniel <166452209+star-Daniels@users.noreply.github.com>
2025-02-08 14:49:43 -0300

    while feito

--- Tarefas.java
+++ Tarefas.java
@@ -1,5 +1,46 @@
 package Gerenciador_de_Tarefas;
 
+import java.util.Scanner;
+
 public class Tarefas {
-    
+    public static void main(String[] args) {
+        
+        boolean cond = true;
+
+        while (cond) {
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.println("Oque DeseJa Fazer\n"
+            +"1- Adicionar tarefa\n"
+            +"2- Marcar como concluida\n"
+            +"3- Listar tarefas\n"
+            +"4- Finalizar sessão\n");
+
+            int escolha = scanner.nextInt();
+            scanner.nextLine();//consume a quebra de linha para não dar problema
+
+            switch (escolha) {
+                case 1:
+                    
+                    break;
+                
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    System.out.println("sessao finalizada");
+                    cond = false;
+
+                    break;
+                default:
+                    System.out.println("Opcao invalida");
+                    break;
+            }
+
+            
+        }
+    }    
 }
