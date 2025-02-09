1bca6308351720c40afc07c1863f387e976be80c
Daniel <166452209+star-Daniels@users.noreply.github.com>
2025-02-09 13:37:41 -0300

    listar

--- Tarefas.java
+++ Tarefas.java
@@ -1,5 +1,7 @@
 package Gerenciador_de_Tarefas;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -12,14 +14,14 @@ public class Tarefas {
         while (cond) {
             Scanner scanner = new Scanner(System.in);
             
-            System.out.println("Oque DeseJa Fazer\n\n"
-            +"1- Adicionar tarefa\n"
-            +"2- Marcar como concluida\n"
-            +"3- Listar tarefas\n"
-            +"4- Finalizar sessão\n");
+            System.out.println("\n\n-----|Oque Deseja Fazer|-----\n\n"
+            +"|1- Adicionar tarefa--------|\n"
+            +"|2- Marcar como concluida---|\n"
+            +"|3- Listar tarefas----------|\n"
+            +"|4- Finalizar sessão--------|\n");
 
             int escolha = scanner.nextInt();
-            scanner.nextLine();//consume a quebra de linha para não dar problema
+            scanner.nextLine();//consome a quebra de linha para não dar problema
 
             switch (escolha) {
                 case 1:
@@ -30,15 +32,15 @@ public class Tarefas {
 
                     break;
                 case 3:
-
+                    listar_tarefas();
                     break;
                 case 4:
-                    System.out.println("sessao finalizada");
+                    System.out.println("----Sessão finalizada----");
                     cond = false;
 
                     break;
                 default:
-                    System.out.println("Opcao invalida");
+                    System.out.println("<----Opção invalida---->");
                     break;
             }
 
@@ -53,19 +55,38 @@ public class Tarefas {
             
             Scanner scan = new Scanner(System.in);
             FileWriter escrever = new FileWriter("tarefas.txt", true); 
-            System.out.println("Escreva a nova tarefa");
+            System.out.println("\n\n----Escreva a nova tarefa----");
             String tarefa = scan.nextLine();
             
             escrever.write(tarefa + "\n");
             escrever.close();
-            System.out.println("\nTarefas salvas com sucesso!\n");
+            System.out.println("\n<----Tarefas salvas com sucesso!---->");
             
         } catch (IOException e) {
-            System.out.println("Erro ao escrever no arquivo: " );
+            System.out.println("<----Erro ao escrever no arquivo---->" );
         }
         
         
 
+    }
+
+    private static void listar_tarefas(){
+        
+        System.out.println("\n\n----Aqui estão suas tarefas----\n\n");
+        try{
+            BufferedReader leitor = new BufferedReader(new FileReader("tarefas.txt"));
+            String ler;
+
+            while ((ler = leitor.readLine()) != null ) {
+                System.out.println(ler);
+                
+            }
+            leitor.close();
+            
+        } catch(IOException e){
+            System.out.println("\n\n<----Houve um problema ao listar as tarefas---->");
+            System.out.println("<----Verifique se você ja criou uma tarefa----->\n\n");
+        }
     }
 
 }
